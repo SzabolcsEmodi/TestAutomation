9869fe4b4355a6d1c7cfacc5fdfe65ced5b2d110
Szabolcs Emodi <es0002@gmail.com>
2015-07-28 19:43:45 +0200

    Create project

---
+++ src/test/java/hu/elte/bead/BeadCucumberTestRunner.java
@@ -0,0 +1,12 @@
+package hu.elte.bead;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Runner for Cucumber tests
+ * @author Szabolcs Emodi
+ */
+@RunWith(Cucumber.class)
+public class BeadCucumberTestRunner {
+}
